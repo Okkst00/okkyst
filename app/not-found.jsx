0d485864461a3aaaf04562ea2185b428e34a985b
Okkst00 <septyantookky19@gmail.com>
2024-05-31 22:54:41 +0700

    not found page revision

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -20,7 +20,7 @@ export default function NotFound() {
               className=" btn-show text-center px-5 py-3 rounded-5"
               href={"/"}
             >
-              <i className="bi bi-arrow-down-circle-fill"></i> Kembali
+              <i className="bi bi-arrow-down-circle-fill"></i> Go Back
             </Link>
           </div>
         </div>
